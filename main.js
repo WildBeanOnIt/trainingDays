fe8e42f5473cbb9fc10e14d992a4f42705698595
Julio Aguilera Jr <julioaguilera337@gmail.com>
2024-05-07 19:08:17 -0500

    fixed the random scope.

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // The scope of `random` is too loose
-const random = Math.floor(Math.random() * 3);
 
 const getRandEvent = () => {
+    const random = Math.floor(Math.random() * 3);
     if (random === 0) {
         return "Marathon";
     } else if (random === 1) {
@@ -28,19 +28,24 @@ const getTrainingDays = (event) => {
 };
 
 // The scope of `name` is too tight
-const logEvent = (name2) => {
-    const name = "Nala";
+const logEvent = (name, event) => {
     console.log(`${name}'s event is: ${event}`);
 };
 
-const logTime = (name2) => {
-    const name = "Nala";
+const logTime = (name, days) => {
     console.log(`${name}'s time to train is: ${days} days`);
 };
 
 const event = getRandEvent();
 const days = getTrainingDays(event);
+const name = "Nala";
 // Define a `name` variable. Use it as an argument after updating logEvent and logTime
-let name2 = "alsdfj";
-logEvent(name2);
-logTime(name2);
+logEvent(name, event);
+logTime(name, days);
+
+const event2 = getRandEvent();
+const days2 = getTrainingDays(event2);
+const name2 = 'Warren';
+ 
+logEvent(name2, event2);
+logTime(name2, days2);
